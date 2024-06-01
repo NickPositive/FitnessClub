3d4a9cec1dc2a0e1ec5fa52ddf8f9b38870126e5
NickPositive <nikita.makcimchuk@gmail.com>
2024-06-02 00:05:30 +0300

    1.8 - Заняття та попапи - Створено сторінку з доступними видами занять - До всіх сторінок з замовленнями створено модали   для замовлення цих самих послуг

---
+++ subscriptions/index.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.getElementById('modal');
+    const modalContentH3 = document.getElementById('modal-h3');
+    const modalContentH2 = document.getElementById('modal-h2');
+    const closeBtn = document.querySelector('.close');
+    const buyButtons = document.querySelectorAll('.card .btn');
+
+    buyButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            event.preventDefault();
+            const card = button.closest('.card');
+            const h3Content = card.querySelector('h3').innerText;
+            const h2Content = card.querySelector('h2').innerText;
+
+            modalContentH3.innerText = h3Content;
+            modalContentH2.innerText = h2Content;
+            modal.style.display = 'block';
+        });
+    });
+
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    });
+
+    // Додаємо обробник події для форми
+    const form = document.querySelector('#modal form');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const name = document.getElementById('name').value;
+        const phone = document.getElementById('phone').value;
+        const modalH3 = modalContentH3.innerText;
+        const modalH2 = modalContentH2.innerText;
+
+        console.log('Інформація з карки:');
+        console.log('Термін', modalH3);
+        console.log('Вартість:', modalH2);
+        console.log('Ім\'я:', name);
+        console.log('Номер телефону:', phone);
+        console.log('');
+
+        // Очищаємо поля форми
+        document.getElementById('name').value = '';
+        document.getElementById('phone').value = '';
+
+        // Закриваємо модальне вікно
+        modal.style.display = 'none';
+    });
+});
---
+++ trainers/index.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.getElementById('modal');
+    const modalTitle = document.getElementById('modal-title');
+    const modalContentH2 = document.getElementById('modal-h2');
+    const closeBtn = document.querySelector('.close');
+    const cardLinks = document.querySelectorAll('.card-container .card');
+
+    cardLinks.forEach(card => {
+        card.addEventListener('click', (event) => {
+            event.preventDefault();
+            const h2Content = card.querySelector('h2.name').innerText;
+
+            modalTitle.innerText = 'Записатись до:';
+            modalContentH2.innerText = h2Content;
+            modal.style.display = 'block';
+        });
+    });
+
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    });
+
+    // Додаємо обробник події для форми
+    const form = document.querySelector('#modal form');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const name = document.getElementById('name').value;
+        const phone = document.getElementById('phone').value;
+        const modalH2 = modalContentH2.innerText;
+
+        console.log('Запис до тренера:');
+        console.log('Тренер:', modalH2);
+        console.log('Ім\'я:', name);
+        console.log('Номер телефону:', phone);
+        console.log('');
+
+        // Очищаємо поля форми
+        document.getElementById('name').value = '';
+        document.getElementById('phone').value = '';
+
+        // Закриваємо модальне вікно
+        modal.style.display = 'none';
+    });
+});
